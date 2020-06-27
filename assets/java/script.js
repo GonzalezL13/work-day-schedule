adcb0febf746f6370e9ee331d3062ec5d6304633
GonzalezL13 <luisgonzalez1331@icloud.com>
2020-06-27 05:15:35 -0700

    saveBtn function added and saves to local storage

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,1 +1,9 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
+//listen to click on save button
+$(".saveBtn").on("click", function() {
+    //get time and text area value
+    var value = $(this).siblings("description").val();
+    var time = $(this).parent().attr("id");
+    
+    localStorage.setItem(time, value);
+});
