8962c376913f3ae91ab18ffeb5c1c7adb56bfe81
GonzalezL13 <luisgonzalez1331@icloud.com>
2020-06-27 11:18:36 -0700

    can load data from local storage

--- assets/java/script.js
+++ assets/java/script.js
@@ -2,8 +2,19 @@ $("#currentDay").text(moment().format("dddd, MMMM Do"));
 //listen to click on save button
 $(".saveBtn").on("click", function() {
     //get time and text area value
-    var value = $(this).siblings("description").val();
+    var value = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
     
     localStorage.setItem(time, value);
 });
+
+//load data from localStorage
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour1 .description").val(localStorage.getItem("hour1"));
+$("#hour2 .description").val(localStorage.getItem("hour2"));
+$("#hour3 .description").val(localStorage.getItem("hour3"));
+$("#hour4 .description").val(localStorage.getItem("hour4"));
+$("#hour5 .description").val(localStorage.getItem("hour5"));
