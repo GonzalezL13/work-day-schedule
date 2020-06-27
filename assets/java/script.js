a63474a68e000aaa2fc23c12d4fbdac95371ad47
GonzalezL13 <luisgonzalez1331@icloud.com>
2020-06-27 13:34:26 -0700

    added time block loop

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,10 +1,10 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 //listen to click on save button
-$(".saveBtn").on("click", function() {
+$(".saveBtn").on("click", function () {
     //get time and text area value
     var value = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
-    
+
     localStorage.setItem(time, value);
 });
 
@@ -18,3 +18,25 @@ $("#hour2 .description").val(localStorage.getItem("hour2"));
 $("#hour3 .description").val(localStorage.getItem("hour3"));
 $("#hour4 .description").val(localStorage.getItem("hour4"));
 $("#hour5 .description").val(localStorage.getItem("hour5"));
+
+//get the current hour
+var currentTime = moment().hours();
+
+//time block loop
+$(".time-block").each(function () {
+    var blockTime = parseInt($(this).attr("id").split("-")[1]);
+
+    // check if we've moved past this time
+    if (blockTime < currentTime) {
+        $(this).addClass("past");
+    }
+    else if (blockTime === currentTime) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+    }
+    else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    }
+});
